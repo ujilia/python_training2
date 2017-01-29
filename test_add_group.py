1004033317289e1d81f9c1e089f89945bd9aece9
Julia <ujilia@gmail.com>
2017-01-29 12:22:17 +0100

    Added test for an empty group creation

--- test_add_group.py
+++ test_add_group.py
@@ -26,6 +26,15 @@ class test_add_group(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
