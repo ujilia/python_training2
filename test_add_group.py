3426f1635faabeaacd316145371b86544fa97a22
Julia <ujilia@gmail.com>
2017-01-30 14:47:06 +0100

    Remove test classes

--- test_add_group.py
+++ test_add_group.py
@@ -1,31 +1,23 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-    
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="2222", header="222", footer="222"))
-        self.app.logout()
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="2222", header="222", footer="222"))
+    app.logout()
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
