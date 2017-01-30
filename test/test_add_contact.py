cb4344e82c24b5a1d88d5ce69efd4c0ada973351
Julia <ujilia@gmail.com>
2017-01-30 19:56:32 +0100

    Remove fixture from test_add_contact and test_add_group

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from fixture.application import Application
 from model.contact import Contact
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(firstname="fsff", middlename="fsf", lastname="fsdf", nickname="sdff", title="dsfdsf", company="fsfdfdf", address="4242", home="3424", mobile="2344", work="4234",
-                               fax="43244", email="434234", email2="3424", email3="4234", homepage="423424", byear="1199", ayear="2423", address2="324eeee", phone2="324rew",
-                               notes="324erwsd"))
+    app.contact.create(
+        Contact(firstname="fsff", middlename="fsf", lastname="fsdf", nickname="sdff", title="dsfdsf", company="fsfdfdf",
+                address="4242", home="3424", mobile="2344", work="4234",
+                fax="43244", email="434234", email2="3424", email3="4234", homepage="423424", byear="1199",
+                ayear="2423", address2="324eeee", phone2="324rew",
+                notes="324erwsd"))
     app.contact.return_to_home_page()
     app.session.logout()
