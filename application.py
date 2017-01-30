29f68829306c7643e28d6e086f38d808f572a8aa
Julia <ujilia@gmail.com>
2017-01-30 14:13:02 +0100

    Add fixture

--- application.py
+++ application.py
@@ -1,30 +1,11 @@
-# -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import unittest
-from group import Group
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
+class Application:
+    def __init__(self):
         self.wd = WebDriver(firefox_binary=FirefoxBinary("/Applications/FirefoxESR.app/Contents/MacOS/firefox-bin"))
         self.wd.implicitly_wait(60)
-    
-    def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="2222", header="222", footer="222"))
-        self.logout()
-
-    def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
 
     def logout(self):
         wd = self.wd
@@ -62,8 +43,5 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def tearDown(self):
+    def destroy(self):
         self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
