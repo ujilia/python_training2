05134646405b1c786e1e0c04895970cdb92d1802
Julia <ujilia@gmail.com>
2017-02-05 13:36:32 +0100

    Optimize filling in forms in test_modify_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,8 +2,20 @@
 from model.group import Group
 
 
-def test_modify_first_group(app):
+# def test_modify_first_group(app):
+#   app.session.login(username="admin", password="secret")
+#   app.group.modify_first_group(Group(name="new", header="change", footer="new change"))
+#   app.group.return_to_groups_page()
+#   app.session.logout()
+
+
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="new", header="change", footer="new change"))
-    app.group.return_to_groups_page()
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
