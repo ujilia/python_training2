7dd12f0c31052f97b42a93ea5184af59c4e37c0f
Julia <ujilia@gmail.com>
2017-02-25 14:30:40 +0100

    Add lists comparison to group tests

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from sys import maxsize
 
 
 def test_add_group(app):
-    app.group.create(Group(name="2222", header="222", footer="222"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="2222", header="222", footer="222")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+    old_groups = app.group.get_group_list()
+    group = Group(name="2222", header="222", footer="222")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
