64d48671dfc7746755bfb613481f3413a58deb51
Julia <ujilia@gmail.com>
2017-02-26 11:43:40 +0100

    Add hash to tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,7 @@ def test_add_contact(app):
                       ayear="2423", address2="324eeee", phone2="324rew",
                       notes="324erwsd")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
