f0bc2dfe282a6588c20de4d108bf6eeec12969eb
Julia <ujilia@gmail.com>
2017-02-26 16:32:12 +0100

    Add randomization to tests

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,7 +1,8 @@
 from model.contact import Contact
+from random import randrange
 
 
-def test_delete_first_contact(app):
+def test_delete_some_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="fsff", middlename="fsf", lastname="fsdf", nickname="sdff", title="dsfdsf", company="fsfdfdf",
                 address="4242", home="3424", mobile="2344", work="4234",
@@ -9,8 +10,9 @@ def test_delete_first_contact(app):
                 ayear="2423", address2="324eeee", phone2="324rew",
                 notes="324erwsd"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.delete_first_contact()
+    index = randrange(len(old_contacts))
+    app.contact.delete_contact_by_index(index)
     assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0:1] = []
+    old_contacts[index:index + 1] = []
     assert old_contacts == new_contacts
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+from random import randrange
 
 
 def test_modify_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="first firstname", lastname="first lastname"))
     old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
     contact = Contact(firstname="new firstname", lastname="new lastname")
-    contact.id = old_contacts[0].id
-    app.contact.modify(contact)
+    contact.id = old_contacts[index].id
+    app.contact.modify_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
