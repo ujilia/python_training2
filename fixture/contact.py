ba6bcd8bd038bca54abcb93beaa643bf64aacd8f
Julia <ujilia@gmail.com>
2017-02-28 20:32:33 +0100

    Add  all_phones to fixture and model

--- fixture/contact.py
+++ fixture/contact.py
@@ -107,9 +107,9 @@ class ContactHelper:
                 firstname = cells[2].text
                 lastname = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text.splitlines()
-                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id, home=all_phones[0],
-                                                  mobile=all_phones[1], work=all_phones[2], phone2=all_phones[3]))
+                all_phones = cells[5].text
+                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id,
+                                                  all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
