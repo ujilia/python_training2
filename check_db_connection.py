4ae71d904edf72b831b6ff368e971842bdf1a06e
Julia <ujilia@gmail.com>
2017-03-06 13:27:03 +0100

    Changed port

--- check_db_connection.py
+++ check_db_connection.py
@@ -2,10 +2,10 @@ import mysql.connector
 # import pymysql.cursors
 
 # connection with mysql.connector
-connection = mysql.connector.connect(host="127.0.0.1", port="8889", database="addressbook", user="root", password="root")
+connection = mysql.connector.connect(host="127.0.0.1", port=3306, database="addressbook", user="root", password="root")
 
 # connection with PyMySQL
-# connection = pymysql.connect(host="127.0.0.1", port="8889", database="addressbook", user="root", password="root")
+# connection = pymysql.connect(host="127.0.0.1", port=3306, database="addressbook", user="root", password="root")
 
 try:
     cursor = connection.cursor()
