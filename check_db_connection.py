dd0cdfc2560c4fa69f7660ca19ae7792427f8eea
Julia <ujilia@gmail.com>
2017-03-19 17:42:48 +0100

    Modified connection method

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,16 +1,17 @@
-import mysql.connector
+from fixture.db import DbFixture
+# import mysql.connector
 # import pymysql.cursors
 
 # connection with mysql.connector
-connection = mysql.connector.connect(host="127.0.0.1", port=3306, database="addressbook", user="root", password="root")
+db = DbFixture(host="127.0.0.1", port=3306, name="addressbook", user="root", password="root")
 
 # connection with PyMySQL
 # connection = pymysql.connect(host="127.0.0.1", port=3306, database="addressbook", user="root", password="root")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
 finally:
-    connection.close()
+    db.destroy()
