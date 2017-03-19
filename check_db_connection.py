a1c1dc84a39783f954d0d793734f63fba4e73268
Julia <ujilia@gmail.com>
2017-03-19 19:08:02 +0100

    Added ORMFixture to check_db_connection

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,17 +1,30 @@
-from fixture.db import DbFixture
+# from fixture.db import DbFixture
 # import mysql.connector
 # import pymysql.cursors
+from fixture.orm import ORMFixture
 
 # connection with mysql.connector
-db = DbFixture(host="127.0.0.1", port=3306, name="addressbook", user="root", password="root")
+# db = DbFixture(host="127.0.0.1", port=3306, name="addressbook", user="root", password="root")
 
 # connection with PyMySQL
 # connection = pymysql.connect(host="127.0.0.1", port=3306, database="addressbook", user="root", password="root")
 
+# try:
+#     contacts = db.get_contact_list()
+#     for contact in contacts:
+#         print(contact)
+#     print(len(contacts))
+# finally:
+#     db.destroy()
+
+
+db = ORMFixture(host="127.0.0.1", port=3306, name="addressbook", user="root", password="root")
+
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass # db.destroy()
+
