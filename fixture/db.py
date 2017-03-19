d73187338d7f05214f57f182b088e636d77afaca
Julia <ujilia@gmail.com>
2017-03-19 19:51:00 +0100

    Added parameters to get_contact_list method

--- fixture/db.py
+++ fixture/db.py
@@ -29,10 +29,13 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, home, mobile, work, phone2, email, email2, email3, address "
+                           "from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, home, mobile, work, phone2, email, email2, email3, address) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, home=home, mobile=mobile,
+                                    work=work, phone2=phone2, email=email, email2=email2, email3=email3,
+                                    address=address))
         finally:
             cursor.close()
         return list
