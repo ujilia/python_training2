f10dc0ae09334fa0021c4c00a7aeb78fe312c016
Julia <ujilia@gmail.com>
2017-03-19 17:41:16 +0100

    Added assertion for DB data + check_ui

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,18 +1,21 @@
 from model.contact import Contact
 from random import randrange
+import random
 
 
-def test_delete_some_contact(app):
-    if app.contact.count() == 0:
+def test_delete_some_contact(app, db, check_ui):
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="fsff", middlename="fsf", lastname="fsdf", nickname="sdff", title="dsfdsf", company="fsfdfdf",
                 address="4242", home="3424", mobile="2344", work="4234",
                 fax="43244", email="434234", email2="3424", email3="4234", homepage="423424", byear="1199",
                 ayear="2423", address2="324eeee", phone2="324rew",
                 notes="324erwsd"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    app.contact.delete_contact_by_index(index)
-    assert len(old_contacts) - 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index:index + 1] = []
-    assert old_contacts == new_contacts
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.delete_contact_by_id(contact.id)
+    old_contacts.remove(contact)
+    new_contacts = db.get_contact_list()
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == \
+               sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 from random import randrange
+import random
 
 
-def test_modify_contact(app):
-    if app.contact.count() == 0:
+def test_modify_contact(app, db, check_ui):
+    if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="first firstname", lastname="first lastname"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact = Contact(firstname="new firstname", lastname="new lastname")
-    contact.id = old_contacts[index].id
-    app.contact.modify_contact_by_index(index, contact)
-    assert len(old_contacts) == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index] = contact
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    contact.firstname = "Changed firstname"
+    contact.lastname = "Changed lastname"
+    app.contact.modify_contact_by_id(contact.id, contact)
+    new_contacts = db.get_contact_list()
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == \
+               sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
