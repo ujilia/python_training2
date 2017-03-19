de8ee6b97e506b705c5ffb909306e06427a45ce0
Julia <ujilia@gmail.com>
2017-03-19 19:07:11 +0100

    Added orm fixture

---
+++ fixture/orm.py
@@ -0,0 +1,51 @@
+from pony.orm import *
+from datetime import datetime
+from model.group import Group
+from model.contact import Contact
+from pymysql.converters import encoders, decoders, convert_mysql_timestamp
+
+
+class ORMFixture:
+
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        firstname = Optional(str, column='firstname')
+        lastname = Optional(str, column='lastname')
+        deprecated = Optional(datetime, column='deprecated')
+
+    def __init__(self, host, port, name, user, password):
+        conv = encoders
+        conv.update(decoders)
+        conv[datetime] = convert_mysql_timestamp
+        self.db.bind('mysql', host=host, port=port, database=name, user=user, password=password, conv=conv)
+        self.db.generate_mapping()
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+        return list(map(convert, groups))
+
+    @db_session
+    def get_group_list(self):
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(с for с in ORMFixture.ORMContact if c.deprecated is None))
+
+
